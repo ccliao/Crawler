c9aa0dd73f48fbf5afcc349237e3a0159c00e1f1
ccliao <ccliao@users.noreply.github.com>
2016-05-12 14:12:48 +0800

    增加reactions&comment用戶清單

--- getfbpost.py
+++ getfbpost.py
@@ -127,16 +127,96 @@ def getdaterange():
         if since == '':
             pass
         else:
-            datetime.datetime.strptime(since, '%Y/%m/%d')
+            since = datetime.datetime.strptime(since, '%Y/%m/%d')
+            since = int(time.mktime(datetime.datetime.timetuple(since)))
+            # since = int((since - datetime.datetime(1970, 1, 1)) / datetime.timedelta(seconds=1))
+
         if until == '':
             pass
         else:
-            until = datetime.datetime.strptime(until, '%Y/%m/%d') + datetime.timedelta(days=1)
-            until = time.mktime(datetime.datetime.timetuple(until))
+            until = datetime.datetime.strptime(until, '%Y/%m/%d') + datetime.timedelta(seconds=86399)
+            until = int(time.mktime(datetime.datetime.timetuple(until)))
+            # until = int((until - datetime.datetime(1970, 1, 1)) / datetime.timedelta(seconds=1))
     except:
         print ('something wrong, type again.')
         getdaterange()
     return (since, until)
+
+def getreactionslist(fbname, since, until, ACCESSTOKEN):
+    #取得reactions名單
+    url = 'https://graph.facebook.com/v2.6/{}/posts?fields=id%2Creactions.limit(100) \
+            &limit=100&since={}&until={}&access_token={}'.format(fbname, since, until, ACCESSTOKEN)
+    while url != '':
+        d = requests.get(url).json()
+        for i in d['data']:
+            if 'reactions' in i:
+                r = i['reactions']
+                while 1:
+                    post_id, user_id, user_name, reaction_type = [], [], [], []
+                    if r['data'] == []:
+                        break
+                    else:
+                        for j in r['data']:
+                            post_id.append(i['id'])
+                            user_id.append(j['id'])
+                            user_name.append(j['name'])
+                            reaction_type.append(j['type'])
+                        reaction_list = pd.DataFrame({'post_id':post_id, 'user_id':user_id, 'user_name':user_name, 'reaction_type':reaction_type})
+                        savefilename = fbname+"_FB-reactionlist-v"+datetime.datetime.now().strftime('%Y%m%d')+".csv"
+                        if not os.path.isfile(savefilename):
+                            reaction_list.to_csv(savefilename)
+                        else:
+                            reaction_list.to_csv(savefilename, mode='a', header=False)
+                        if 'next' not in r['paging']:
+                            break
+                        else:
+                            nexturl = r['paging']['next']
+                            r = requests.get(nexturl).json()
+        if 'paging' in d:
+            if 'next' in d['paging']:
+                url = d['paging']['next']
+            else:
+                url = ''
+        else:
+            url = ''
+    print('Get Reactions List Finished')
+
+def getcommentslist(fbname, since, until, ACCESSTOKEN):
+    # 取得comments名單
+    url = 'https://graph.facebook.com/v2.6/{}/posts?fields=id%2Ccomments.limit(100) \
+            &limit=100&since={}&until={}&access_token={}'.format(fbname, since, until, ACCESSTOKEN)
+    while url != '':
+        d = requests.get(url).json()
+        for i in d['data']:
+            if 'comments' in i:
+                r = i['comments']
+                while 1:
+                    post_id, user_id, user_name, reaction_type = [], [], [], []
+                    for j in r['data']:
+                        post_id.append(i['id'])
+                        user_id.append(j['from']['id'])
+                        user_name.append(j['from']['name'])
+                        reaction_type.append('comment')
+                    reaction_list = pd.DataFrame(
+                        {'post_id': post_id, 'user_id': user_id, 'user_name': user_name, 'reaction_type': reaction_type})
+                    savefilename = fbname + "_FB-reactionlist-v" + datetime.datetime.now().strftime('%Y%m%d') + ".csv"
+                    if not os.path.isfile(savefilename):
+                        reaction_list.to_csv(savefilename)
+                    else:
+                        reaction_list.to_csv(savefilename, mode='a', header=False)
+                    if 'next' not in r['paging']:
+                        break
+                    else:
+                        nexturl = r['paging']['next']
+                        r = requests.get(nexturl).json()
+        if 'paging' in d:
+            if 'next' in d['paging']:
+                url = d['paging']['next']
+            else:
+                url = ''
+        else:
+            url = ''
+    print('Get Comments List Finished')
 
 if __name__ == '__main__':
     ACCESSTOKEN = input('your facebook access token： ')
@@ -156,5 +236,15 @@ if __name__ == '__main__':
             url = getnextpaging(r)
             i += 1
             print (i)
+    print("Get Post Finished")
 
-    print ("Finished")
+    Go = input('Continue to get Reactions&Comments User List(Y/N)： ')
+    if (Go == "Y") or (Go == "y"):
+        print ("Please wait to get Reactions List")
+        getreactionslist(fbname, since, until, ACCESSTOKEN)
+        print("Please wait for get Comments List")
+        getcommentslist(fbname, since, until, ACCESSTOKEN)
+        print("Totally Finished")
+    else:
+        print ("Totally Finished")
+
