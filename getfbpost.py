ce3a082d95537eb7934fb2e8d75fe74af5bb0b28
ccliao <ccliao@users.noreply.github.com>
2016-05-21 21:34:42 +0800

    增加goo.gl及bit.ly短網址的點擊統計資訊

--- getfbpost.py
+++ getfbpost.py
@@ -7,60 +7,128 @@ import time
 import re
 # from dateutil import tz
 
-def getdata(r, key):
+def getdata(r, gtoken, btoken):
     jd = json.loads(r.text)['data']
     df = pd.read_json(json.dumps(jd))
 
     # 貼文內容
     pmessage, pname, link, permalink_url = [], [], [], []
-    short_url, original_url, shorturl_createdtime, click_count, referrer, platform, country, browser = [], [], [], [], [], [], [], []
+    short_url, original_url, click_count, referrer, platform, country, browser = [],[],[],[],[],[],[]
+    short_url2, original_url2, click_count2, referrer2, country2, platform2, browser2 = [],[],[],[],[],[],[]
     if 'message' in df:
-        for i in df['message']:
+        for index,i in enumerate(df['message']):
             pmessage.append(i)
-            # 取得短網址
-            if key == '':
+            shorturls = re.findall("http[s]*://(?:goo.gl|bit.ly)/[A-Z a-z 0-9]+", i)
+            shorturls2 = re.findall("http[s]*://(?:goo.gl|bit.ly)/[A-Z a-z 0-9]+", df['link'][index])
+            shorturls = shorturls + shorturls2
+            n = 0
+            if shorturls != []:
+                s = len(shorturls)
+                for shorturl in shorturls:
+                    n += 1
+                    if n == 1:
+                        if 'goo.gl' in shorturl:
+                            s_url, o_url, c_count, refer, ct, pf, bs = getgoogleshorturl(shorturl, gtoken)
+                            short_url.append(' , '.join(j for j in s_url))
+                            original_url.append(' , '.join(j for j in o_url))
+                            click_count.append(' , '.join(str(j) for j in c_count))
+                            referrer.append(' , '.join(j for j in refer))
+                            country.append(' , '.join(j for j in ct))
+                            platform.append(' , '.join(j for j in pf))
+                            browser.append(' , '.join(j for j in bs))
+                            if s == 1:
+                                short_url2.append('')
+                                original_url2.append('')
+                                click_count2.append('')
+                                referrer2.append('')
+                                country2.append('')
+                                platform2.append('')
+                                browser2.append('')
+                        elif 'bit.ly' in shorturl:
+                            s_url, o_url, c_count, refer, ct, pf, bs = getbitlyshorturl(shorturl, btoken)
+                            short_url.append(' , '.join(j for j in s_url))
+                            original_url.append(' , '.join(j for j in o_url))
+                            click_count.append(' , '.join(str(j) for j in c_count))
+                            referrer.append(' , '.join(j for j in refer))
+                            country.append(' , '.join(j for j in ct))
+                            platform.append(' , '.join(j for j in pf))
+                            browser.append(' , '.join(j for j in bs))
+                            if s == 1:
+                                short_url2.append('')
+                                original_url2.append('')
+                                click_count2.append('')
+                                referrer2.append('')
+                                country2.append('')
+                                platform2.append('')
+                                browser2.append('')
+                        else:
+                            short_url.append('')
+                            original_url.append('')
+                            click_count.append('')
+                            referrer.append('')
+                            country.append('')
+                            platform.append('')
+                            browser.append('')
+                    elif n == 2:
+                        if 'goo.gl' in shorturl:
+                            s_url, o_url, c_count, refer, ct, pf, bs = getgoogleshorturl(shorturl, gtoken)
+                            short_url2.append(' , '.join(j for j in s_url))
+                            original_url2.append(' , '.join(j for j in o_url))
+                            click_count2.append(' , '.join(str(j) for j in c_count))
+                            referrer2.append(' , '.join(j for j in refer))
+                            country2.append(' , '.join(j for j in ct))
+                            platform2.append(' , '.join(j for j in pf))
+                            browser2.append(' , '.join(j for j in bs))
+                        elif 'bit.ly' in shorturl:
+                            s_url, o_url, c_count, refer, ct, pf, bs = getbitlyshorturl(shorturl, btoken)
+                            short_url2.append(' , '.join(j for j in s_url))
+                            original_url2.append(' , '.join(j for j in o_url))
+                            click_count2.append(' , '.join(str(j) for j in c_count))
+                            referrer2.append(' , '.join(j for j in refer))
+                            country2.append(' , '.join(j for j in ct))
+                            platform2.append(' , '.join(j for j in pf))
+                            browser2.append(' , '.join(j for j in bs))
+                        else:
+                            short_url2.append('')
+                            original_url2.append('')
+                            click_count2.append('')
+                            referrer2.append('')
+                            country2.append('')
+                            platform2.append('')
+                            browser2.append('')
+                    else:
+                        print (str(n)+'-have more shorturl!!!')
+            else:
                 short_url.append('')
                 original_url.append('')
-                # shorturl_createdtime.append('')
                 click_count.append('')
                 referrer.append('')
-                platform.append('')
                 country.append('')
+                platform.append('')
                 browser.append('')
-            else:
-                shorturl = re.findall("http[s]*://goo.gl/[A-Z a-z 0-9]+", i)
-
-                # 取得短網址相關資訊
-                if shorturl == []:
-                    short_url.append('')
-                    original_url.append('')
-                    # shorturl_createdtime.append('')
-                    click_count.append('')
-                    referrer.append('')
-                    platform.append('')
-                    country.append('')
-                    browser.append('')
-                else:
-                    originalurl, shorturlcreatedtime, clickcount_all, referrer_all, platform_all, country_all, browser_all = getgoogleshorturl(
-                        key, shorturl[0].strip())
-                    short_url.append(' , '.join(j for j in shorturl))
-                    original_url.append(' , '.join(j for j in originalurl))
-                    # shorturl_createdtime.append(' , '.join(j for j in shorturlcreatedtime))
-                    click_count.append(' , '.join(j for j in clickcount_all))
-                    referrer.append(' , '.join(j for j in referrer_all))
-                    platform.append(' , '.join(j for j in platform_all))
-                    country.append(' , '.join(j for j in country_all))
-                    browser.append(' , '.join(j for j in browser_all))
+                short_url2.append('')
+                original_url2.append('')
+                click_count2.append('')
+                referrer2.append('')
+                country2.append('')
+                platform2.append('')
+                browser2.append('')
     else:
         pmessage.append('')
         short_url.append('')
         original_url.append('')
-        # shorturl_createdtime.append('')
         click_count.append('')
         referrer.append('')
-        platform.append('')
         country.append('')
+        platform.append('')
         browser.append('')
+        short_url2.append('')
+        original_url2.append('')
+        click_count2.append('')
+        referrer2.append('')
+        country2.append('')
+        platform2.append('')
+        browser2.append('')
 
     for i in df['name']:
         pname.append(i)
@@ -72,7 +140,7 @@ def getdata(r, key):
     # 調整時區為台灣時間
     created_time = []
     for i in df['created_time']:
-        created_time.append(i.tz_localize('UTC').tz_convert('Asia/Taipei'))
+        created_time.append(i.tz_localize('UTC').tz_convert('Asia/Taipei').strftime('%Y-%m-%d %H:%M:%S %A'))
 
     #貼文按讚數
     reaction_count = []
@@ -111,46 +179,52 @@ def getdata(r, key):
             if i['data'][4]['title'] == 'Lifetime Post Total Reach':
                 Post_Total_Reach.append(i['data'][4]['values'][0]['value'])
             else:
-                Post_Total_Reach.append(0)
+                Post_Total_Reach.append('')
 
             # 貼文點擊次數Lifetime Post Consumptions
             if i['data'][44]['title'] == 'Lifetime Post Consumptions':
                 Post_Consumptions.append(i['data'][44]['values'][0]['value'])
             else:
-                Post_Consumptions.append(0)
+                Post_Consumptions.append('')
 
             # 貼文點擊次數分類Lifetime Post Consumptions by type
             if i['data'][46]['title'] == 'Lifetime Post Consumptions by type':
                 try:
                     Post_Consumptions_other.append(i['data'][46]['values'][0]['value']['other clicks'])
                 except:
-                    Post_Consumptions_other.append(0)
+                    Post_Consumptions_other.append('')
                 try:
                     Post_Consumptions_photo.append(i['data'][46]['values'][0]['value']['photo view'])
                 except:
-                    Post_Consumptions_photo.append(0)
+                    Post_Consumptions_photo.append('')
                 try:
                     Post_Consumptions_link.append(i['data'][46]['values'][0]['value']['link clicks'])
                 except:
-                    Post_Consumptions_link.append(0)
+                    Post_Consumptions_link.append('')
             else:
-                Post_Consumptions_other.append(0)
-                Post_Consumptions_photo.append(0)
-                Post_Consumptions_link.append(0)
+                Post_Consumptions_other.append('')
+                Post_Consumptions_photo.append('')
+                Post_Consumptions_link.append('')
     else:
-        Post_Total_Reach=0
-        Post_Consumptions=0
-        Post_Consumptions_other=0
-        Post_Consumptions_photo=0
-        Post_Consumptions_link=0
+        Post_Total_Reach=''
+        Post_Consumptions=''
+        Post_Consumptions_other=''
+        Post_Consumptions_photo=''
+        Post_Consumptions_link=''
 
-    lion_fb = pd.DataFrame({'postdate': created_time, 'message': pmessage, 'reaction_count': reaction_count, 'share_count': share_count, \
-         'comment': comment_count, 'linkname': pname, 'link': link, 'postlink': permalink_url, 'type':df['type'], \
+    lion_fb = pd.DataFrame(
+        {'postdate': created_time, 'message': pmessage, 'reaction_count': reaction_count, 'share_count': share_count, \
+         'comment_count': comment_count, 'linkname': pname, 'link': link, 'postlink': permalink_url, 'type':df['type'], \
          'Post_Total_Reach': Post_Total_Reach, 'Post_Consumptions': Post_Consumptions, \
          'Post_Consumptions_other': Post_Consumptions_other, 'Post_Consumptions_photo': Post_Consumptions_photo, \
-         'Post_Consumptions_link': Post_Consumptions_link, 'shorturl': short_url, 'originalurl': original_url, \
-         'clickcount_all': click_count, 'referrer_all': referrer, \
-         'platform_all': platform, 'country_all': country, 'browser_all': browser})
+         'Post_Consumptions_link': Post_Consumptions_link, 'shorturl':short_url, 'originalurl':original_url, \
+         'clickcount':click_count, 'referrer':referrer, 'platform':platform, 'country':country, 'browser':browser, \
+         'shorturl2':short_url2, 'originalurl2':original_url2, 'clickcount2':click_count2, 'referrer2':referrer2, \
+         'platform2':platform2, 'country2':country2, 'browser2':browser2},
+        columns = ["postdate","postlink","type","message","link","linkname","reaction_count","share_count","comment_count", \
+                   "Post_Total_Reach","Post_Consumptions","Post_Consumptions_link","Post_Consumptions_photo", \
+                   "Post_Consumptions_other","shorturl","originalurl","clickcount","referrer","country","browser","platform",\
+                   "shorturl2","originalurl2","clickcount2","referrer2","country2","browser2","platform2"])
 
     savefilename = fbname + "_fb-v" + datetime.datetime.now().strftime('%Y%m%d') + ".csv"
     if not os.path.isfile(savefilename):
@@ -265,31 +339,81 @@ def getcommentslist(fbname, since, until, ACCESSTOKEN):
             url = ''
     print('Get Comments List Finished')
 
-def getgoogleshorturl(key, shorturl):
-    gurl = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl={}&projection=FULL&key={}'.format(shorturl, key)
-    req= requests.get(gurl).json()
+def getgoogleshorturl(glink, gtoken):
+    short_url, original_url, click_count, referrer, country, platform, browser = [], [], [], [], [], [], []
+    if gtoken == '':
+        short_url.append('')
+        original_url.append('')
+        click_count.append('')
+        referrer.append('')
+        country.append('')
+        platform.append('')
+        browser.append('')
+    else:
+        time.sleep(1)
+        gurl = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl={}&projection=FULL&key={}'.format(glink, gtoken)
+        req= requests.get(gurl).json()
 
-    #僅先納入alltime,尚有month,week,day,twoHours
-    originalurl,shorturlcreatedtime,clickcount_all,referrer_all,platform_all,country_all,browser_all=[],[],[],[],[],[],[]
-    originalurl.append(req['longUrl']) #原網址
-    # d = datetime.datetime.strptime(req['created'][0:19], "%Y-%m-%dT%H:%M:%S")
-    # d = d.replace(tzinfo=tz.gettz('UTC'))
-    # shorturlcreatedtime.append(str(d.astimezone(tz.gettz('Asia/Taipei')).date()))  #短址設立日期
-    clickcount_all.append(req['analytics']['allTime']['shortUrlClicks']) #短址點擊量
-    for i in req['analytics']['allTime']['referrers']:
-        referrer_all.append(str(i['count'])+' from '+i['id']) #短址點擊量來源
-    for i in req['analytics']['allTime']['platforms']:
-        platform_all.append(str(i['count'])+' from '+i['id']) #短址點擊量平台
-    for i in req['analytics']['allTime']['countries']:
-        country_all.append(str(i['count'])+' from '+i['id']) #短址點擊量國家
-    for i in req['analytics']['allTime']['browsers']:
-        browser_all.append(str(i['count'])+' from '+i['id']) #短址點擊量國家
-    return originalurl,shorturlcreatedtime,clickcount_all,referrer_all,platform_all,country_all,browser_all
+        #僅先納入alltime,尚有month,week,day,twoHours
+        short_url.append(glink) #短網址
+        original_url.append(req['longUrl']) #原網址
+    #     d = datetime.datetime.strptime(req['created'][0:19], "%Y-%m-%dT%H:%M:%S")
+    #     d = d.replace(tzinfo=tz.gettz('UTC'))
+    #     shorturlcreatedtime.append(str(d.astimezone(tz.gettz('Asia/Taipei')).date()))  #短址設立日期
+        click_count.append(req['analytics']['allTime']['shortUrlClicks']) #短址點擊量
+        for i in req['analytics']['allTime']['referrers']:
+            referrer.append(str(i['count'])+' from '+i['id']) #短址點擊量來源
+        for i in req['analytics']['allTime']['platforms']:
+            platform.append(str(i['count'])+' from '+i['id']) #短址點擊量平台
+        for i in req['analytics']['allTime']['countries']:
+            country.append(str(i['count'])+' from '+i['id']) #短址點擊量國家
+        for i in req['analytics']['allTime']['browsers']:
+            browser.append(str(i['count'])+' from '+i['id']) #短址點擊量國家
+    return short_url, original_url, click_count, referrer, country, platform, browser
+
+def getbitlyshorturl(blink, btoken):
+    short_url, original_url, click_count, referrer, country, platform, browser = [], [], [], [], [], [], []
+    if btoken == '':
+        short_url.append('')
+        original_url.append('')
+        click_count.append('')
+        referrer.append('')
+        country.append('')
+        platform.append('')
+        browser.append('')
+    else:
+        #透過bit.ly取得短網址點擊相關資訊
+        burl_ref = 'https://api-ssl.bitly.com/v3/link/referrers?access_token={}&link={}'.format(btoken, blink) #取得點擊來源
+        burl_click = 'https://api-ssl.bitly.com/v3/link/clicks?access_token={}&link={}'.format(btoken, blink) #取得點擊數
+        burl_country = 'https://api-ssl.bitly.com/v3/link/countries?access_token={}&link={}'.format(btoken, blink) #取得點擊國家
+        burl_long = 'https://api-ssl.bitly.com/v3/expand?access_token={}&shortUrl={}'.format(btoken, blink) #取得原網址
+        #與goo.gl相比,無platform與browser來源判讀
+
+        time.sleep(1)
+        r_ref = requests.get(burl_ref).json()
+        time.sleep(1)
+        r_click = requests.get(burl_click).json()
+        time.sleep(1)
+        r_country = requests.get(burl_country).json()
+        time.sleep(1)
+        r_long = requests.get(burl_long).json()
+
+        short_url.append(blink) #短網址
+        original_url.append(r_long['data']['expand'][0]['long_url']) #原網址
+        click_count.append(r_click['data']['link_clicks']) #點擊數
+        for ref in r_ref['data']['referrers']:
+            referrer.append(str(ref['clicks'])+'-'+ref['referrer']) #點擊來源
+        for ref in r_country['data']['countries']:
+            country.append(str(ref['clicks'])+'-'+ref['country']) #點擊來源
+        platform.append('') #bitly無資料但google有
+        browser.append('') #bitly無資料但google有
+    return short_url, original_url, click_count, referrer, country, platform, browser
 
 if __name__ == '__main__':
     ACCESSTOKEN = input('your facebook access token： ')
     fbname = input('your facebook name or id： ')
-    google_key = input('your google api key： ')
+    gtoken = input('your goo.gl api key： ')
+    btoken = input('your bit.ly api key： ')
     since, until = getdaterange()
     url = 'https://graph.facebook.com/v2.6/{}/posts?fields=name%2Cmessage%2Ccreated_time%2Cid%2Cshares%2C \
                     reactions.summary(1)%2Clink%2Cpermalink_url%2Ccomments.summary(1)%2Cinsights%2Ctype \
@@ -300,8 +424,9 @@ if __name__ == '__main__':
         r = requests.get(url)
         if len(json.loads(r.text)['data']) == 0:
             url = ''
+            print ('Please check!!! No Data.')
         else:
-            getdata(r,google_key)
+            getdata(r,gtoken,btoken)
             url = getnextpaging(r)
             i += 1
             print (i)
